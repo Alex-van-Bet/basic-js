3cc12b8e0803530b5d3f312b2bb4f3e66d1b0b32
unknown <C:\Users\Home\AppData\Roaming\The Bat!>
2022-04-28 19:24:08 +0300

    add 2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,13 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY = 15; /**современная активность A*/
+const HALF_LIFE_PERIOD = 5730;/**период полураспада t1/2*/
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
  * 
- * @param {String} sampleActivity string representation of current activity 
+ * @param {String} sampleActivity string representation of current activity текущая активность
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'Number' ){
+    let a = sampleActivity;
+  let dateSample;
+  dateSample = Math.ceil(Math.log(MODERN_ACTIVITY / a ) / ( 0.693 / HALF_LIFE_PERIOD), 1);
+  return dateSample;
+  }
+
+  else {
+    return false;
+ 
+  }
 }
 
 module.exports = {
